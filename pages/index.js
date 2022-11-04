cacc1dd67ae61295bd4620c337424027a99404ae
ladellerby <ladellerby@gmail.com>
2022-11-04 14:01:53 -0700

    adding about us info

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,16 @@ import Services from '../components/services';
 import About from '../components/about';
 import Testimonials from '../components/testimonials';
 import Reservation from '../components/reservation';
+import TestimonialHero from '../components/testimonial-hero';
+import Features from '../components/Features';
+import InfoHero from '../components/InfoHero';
 
 export default function IndexPage() {
   return (
     <div>
       <Hero />
       {/* <DishOfDay /> */}
-
+      <Features />
       <img
         class="max-w-full h-auto mx-auto"
         src="/the-bridge-cafe-menu.png"
