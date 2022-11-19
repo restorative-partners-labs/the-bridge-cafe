20d5ef5a966dbb8a0047f3d5d06a63cb295d94ac
ladellerby <ladellerby@gmail.com>
2022-11-19 02:41:07 -0800

    - adding prismic

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,16 @@
 import '@/styles/tailwind.css'
 import 'focus-visible'
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../../prismicio'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider internalLinkComponent={Link}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  )
 }
