32de0b0089394b4eb128114dcd3df4b9090f4f18
ladellerby <ladellerby@gmail.com>
2023-01-15 16:24:15 -0800

    - add google anayltics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,11 +4,13 @@ import Link from 'next/link'
 import { PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
 import { repositoryName } from '../../prismicio'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
   return (
     <PrismicProvider internalLinkComponent={Link}>
       <PrismicPreview repositoryName={repositoryName}>
+      <GoogleAnalytics trackPageViews />
         <Component {...pageProps} />
       </PrismicPreview>
     </PrismicProvider>
