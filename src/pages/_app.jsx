7e2938ad71f84bbcdc883ed4f6660d36c051f66c
ladellerby <ladellerby@gmail.com>
2023-08-08 22:52:25 -0700

    - adding vercel analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,9 +6,7 @@ import { PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
 import { repositoryName } from '../../prismicio'
 import { GoogleAnalytics, event } from 'nextjs-google-analytics'
-import { useEffect } from 'react'
-import dynamic from 'next/dynamic'
-const {} = dynamic(import('tw-elements'), { ssr: false })
+import { Analytics } from '@vercel/analytics/react'
 
 export function reportWebVitals({ id, name, label, value }) {
   event(name, {
@@ -20,17 +18,12 @@ export function reportWebVitals({ id, name, label, value }) {
 }
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    const use = async () => {
-      ;(await import('tw-elements')).default
-    }
-    use()
-  }, [])
   return (
     <PrismicProvider internalLinkComponent={Link}>
       <PrismicPreview repositoryName={repositoryName}>
         <GoogleAnalytics trackPageViews />
         <Component {...pageProps} />
+        <Analytics />
       </PrismicPreview>
     </PrismicProvider>
   )
