0737da69f72fcbe782069c44440938c432db4d0f
ladellerby <ladellerby@gmail.com>
2023-09-15 07:08:12 -0700

    - add online ordering closed logic back in using a spot on api response to determine the availbility of the online ordering widget.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -78,29 +78,25 @@ export function Header() {
   const [showOnlineOrderClose, setShowOnlineOrderClose] = useState(false)
   const cancelButtonRef = useRef(null)
 
-  const handleOrderOnlineNavigation = () => {
-    // const currentHour = new Date()
-
-    // // Convert PST (UTC-8) to the user's local time (if applicable)
-    // var pstCurrentHour = currentHour.getUTCHours() - 1
-
-    // if (pstCurrentHour > 0) {
-    //   pstCurrentHour = pstCurrentHour + 24
-    // }
-    // if (pstCurrentHour > 24) {
-    //   pstCurrentHour = pstCurrentHour - 24
-    // }
-
-    // console.log(pstCurrentHour)
-
-    // // Check if the current hour is between 3 PM and 7 AM PST
-    // if (pstCurrentHour >= 15) {
-    //   setShowOnlineOrderClose(true)
-    //   return
-    // }
-    router.push(
-      'https://order.spoton.com/so-the-bridge-cafe-10987/san-luis-obispo-ca/63338b3bf3ebec0040438b39'
-    )
+  const handleOrderOnlineNavigation = async () => {
+    try {
+      const response = await fetch('/api/status')
+      const data = await response.json()
+      if (data.onlineOrderingAvailable === true) {
+        // Online ordering is available, so continue to router push.
+        router.push(
+          'https://order.spoton.com/so-the-bridge-cafe-10987/san-luis-obispo-ca/63338b3bf3ebec0040438b39'
+        )
+      } else {
+        // Online ordering is not available, set showOnlineOrderClose to true.
+        setShowOnlineOrderClose(true)
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error)
+      router.push(
+        'https://order.spoton.com/so-the-bridge-cafe-10987/san-luis-obispo-ca/63338b3bf3ebec0040438b39'
+      )
+    }
   }
   return (
     <>
@@ -179,7 +175,9 @@ export function Header() {
                                 <span className="text-2xl">Donate</span>
                               </Button>
                               <Button
-                                href="https://order.spoton.com/so-the-bridge-cafe-10987/san-luis-obispo-ca/63338b3bf3ebec0040438b39"
+                                onClick={() => {
+                                  handleOrderOnlineNavigation()
+                                }}
                                 style={{ background: '#05716c' }}
                               >
                                 <span className="text-2xl">Order</span>
---
+++ src/pages/api/status.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export default async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      const response = await axios.get(
+        'https://order.spoton.com/api/merchants/63338b3bf3ebec0040438b39'
+      )
+
+      if (response.data.schedule) {
+        if (response.data.schedule.closed) {
+          res.status(200).json({ onlineOrderingAvailable: false })
+        } else {
+          res.status(200).json({ onlineOrderingAvailable: true })
+        }
+      } else {
+        res.status(400).json({ onlineOrderingAvailable: true })
+      }
+    } catch (error) {
+      console.error('Error:', error.message)
+      res.status(500).json({ error: 'Internal Server Error' })
+    }
+  }
+}
