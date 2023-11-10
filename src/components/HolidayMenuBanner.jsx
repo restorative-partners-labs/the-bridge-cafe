0c1b7956763508b583aae051731e6b3185a86e6a
ladellerby <ladellerby@gmail.com>
2023-11-10 13:11:42 -0800

    update holiday menu with canva design

--- src/components/HolidayMenuBanner.jsx
+++ src/components/HolidayMenuBanner.jsx
@@ -1,68 +1,41 @@
+import { Button } from '@/components/Button'
+
 export default function HolidayMenuBanner() {
   return (
-    <div className="bg-bridge">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="relative pb-16 pt-48 sm:pb-24">
-          <div className="absolute -top-32 left-1/2 -translate-x-1/2 transform sm:top-6 sm:translate-x-0">
-            <div className="ml-24 flex min-w-max space-x-6 sm:ml-3 lg:space-x-8">
-              <div className="flex space-x-6 sm:flex-col sm:space-x-0 sm:space-y-6 lg:space-y-8">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-64 w-64 rounded-lg object-cover md:h-72 md:w-72"
-                    src="/images/food/turkey.png"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div className="flex space-x-6 sm:-mt-20 sm:flex-col sm:space-x-0 sm:space-y-6 lg:space-y-8">
-                <div className="mt-6 flex-shrink-0 sm:mt-0">
-                  <img
-                    className="h-64 w-64 rounded-lg object-cover md:h-72 md:w-72"
-                    src="/images/food/pie.png"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div className="flex space-x-6 sm:flex-col sm:space-x-0 sm:space-y-6 lg:space-y-8">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-64 w-64 rounded-lg object-cover md:h-72 md:w-72"
-                    src="/images/food/salad.png"
-                    alt=""
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div>
-            <h2
-              id="sale-heading"
-              className="text-xl font-bold tracking-tight text-white md:text-2xl"
+    <div className="relative isolate overflow-hidden bg-gradient-to-b from-bridge pt-14">
+      <div
+        className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-bridge ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+        aria-hidden="true"
+      />
+      <div className="mx-auto max-w-7xl px-6 py-10 sm:py-40 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+          <img
+            src="/holiday-menu.png"
+            alt=""
+            className="mt-10 w-full max-w-lg rounded-2xl object-cover sm:mt-10 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-10"
+          />
+          <div className="mt-10 flex items-center justify-center">
+            <Button
+              href="/cafeclub"
+              variant="outline"
+              color="theme"
+              className="w-56 bg-white"
             >
-              Season of Giving & Gathering
-              <br />
-              Check Out Our Thanksgiving Menu and Order Now!
-              <br />
-              Order Online Before 11/20 @3PM
-            </h2>
-            <div className="mt-6 text-base">
-              <a href="/holiday-menu.png" className="font-semibold text-white">
-                View Menu
-                <span aria-hidden="true"> &rarr;</span>
-              </a>
-            </div>
-            <div className="mt-2 text-base">
-              <a
-                href="https://order.spoton.com/so-the-bridge-cafe-10987/san-luis-obispo-ca/63338b3bf3ebec0040438b39"
-                className="font-semibold text-white"
-              >
-                Order Online
-                <span aria-hidden="true"> &rarr;</span>
-              </a>
-            </div>
+              <span className="ml-2.5">
+                <p className="text-xl">Order Now</p>
+              </span>
+            </Button>
           </div>
+          <h2
+            id="sale-heading"
+            className="text-xl font-bold tracking-tight text-bridge md:text-2xl text-center"
+          >
+            Check Out Our Thanksgiving Menu and Order Now! Order Online Before
+            11/20 @3PM
+          </h2>
         </div>
       </div>
+      <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
     </div>
   )
 }
