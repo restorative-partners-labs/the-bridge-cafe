9165ba5c10ab7231bf1a651031b46e76d1e7f89d
ladellerby <ladellerby@gmail.com>
2023-11-14 18:41:33 -0800

    - add special type

--- sanity/schema.js
+++ sanity/schema.js
@@ -1,8 +1,9 @@
-import {blockContent} from './schemas/blockContent'
-import {category} from './schemas/category'
-import {post} from './schemas/post'
-import {author} from './schemas/author'
+import { blockContent } from './schemas/blockContent'
+import { category } from './schemas/category'
+import { post } from './schemas/post'
+import { author } from './schemas/author'
+import { special } from './schemas/special'
 
 export const schema = {
-  types: [post, author, category, blockContent],
+  types: [post, author, category, blockContent, special],
 }
