0ae64358b0e9df1a5b38bee67e51f2c8ee56da42
ladellerby <ladellerby@gmail.com>
2024-02-05 17:22:17 -0800

    update sanity

---
+++ sanity/sanity.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './schemas'
+
+export default defineConfig({
+  name: 'the-bridge-cafe-admin',
+  title: 'The Bridge Cafe Admin',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: 'production',
+  plugins: [
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title('Base')
+          .items([
+            S.listItem()
+              .title('Site Settings')
+              .child(
+                S.document()
+                  .schemaType('siteSettings')
+                  .documentId('siteSettings')
+              ),
+            ...S.documentTypeListItems(),
+          ]),
+    }),
+  ],
+  schema: {
+    types: schemaTypes,
+  },
+})
