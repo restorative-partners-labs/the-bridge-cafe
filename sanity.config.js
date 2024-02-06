97216da621c30f4f0475aa962188b2001953ce3a
ladellerby <ladellerby@gmail.com>
2024-02-05 17:31:35 -0800

    migrate sanity settings

--- sanity.config.js
+++ sanity.config.js
@@ -2,13 +2,13 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `\src\pages\admin\[[...index]].jsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import { apiVersion, dataset, projectId } from './sanity/env'
+import { schema } from './sanity/schema'
 
 export default defineConfig({
   basePath: '/admin',
@@ -18,8 +18,23 @@ export default defineConfig({
   schema,
   plugins: [
     deskTool(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title('Base')
+          .items([
+            S.listItem()
+              .title('Site Settings')
+              .child(
+                S.document()
+                  .schemaType('siteSettings')
+                  .documentId('siteSettings')
+              ),
+            ...S.documentTypeListItems().filter(
+              (listItem) => !['siteSettings'].includes(listItem.getId())
+            ),
+          ]),
+    }),
+    visionTool({ defaultApiVersion: apiVersion }),
   ],
 })
